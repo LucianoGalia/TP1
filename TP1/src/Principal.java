97c03772330a98a7aff00433ab23a9e6a62119c4
estudiante <nahuelbs95@gmail.com>
2023-09-05 16:54:49 -0300

    Guardo el TP1 hasta el punto 8

---
+++ TP1/src/Principal.java
@@ -0,0 +1,45 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+import java.util.ArrayList;
+/**
+ *
+ * @author estudiante
+ */
+public class Principal {
+    public static void main(String[] args) {
+        
+        /*
+        int entero = 10;
+        float decimal = 10.2f;
+        double doble = 100.4;
+        String cadena = "Ventilador";      
+        
+        System.out.println(entero);
+        System.out.println(decimal);
+        System.out.println(doble);
+        System.out.println(cadena); */
+        
+        /*ArrayList<Float> lista = new ArrayList<>();
+        
+        float numero1 = 2.4f;
+        float numero2 = 2.8f;
+        float promedio;
+        float total;
+        lista.add(numero1);
+
+        lista.add(numero2);
+        
+        for(Float f : lista) {
+            System.out.println(f);
+        }
+        total = numero1 + numero2;
+        promedio = total / 2;
+        
+        System.out.println("\n el promedio es: " + promedio);*/
+        
+        
+    }
+            
+}
---
+++ TP1/src/paqClases/PrimeraClase.java
@@ -0,0 +1,49 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package paqClases;
+import java.util.ArrayList;
+/**
+ *
+ * @author estudiante
+ */
+public class PrimeraClase {
+    
+    String unaCadena;
+    int unEntero;
+    
+    public static void main(String[] args){
+        
+        PrimeraClase unObjeto1 = new PrimeraClase();
+        PrimeraClase unObjeto2 = new PrimeraClase();
+        PrimeraClase unObjeto3 = new PrimeraClase();
+        PrimeraClase unObjeto4 = new PrimeraClase();
+        PrimeraClase unObjeto5 = new PrimeraClase();
+        
+        unObjeto1.unaCadena = "Ventilador";
+        unObjeto1.unEntero = 5;
+        unObjeto2.unaCadena = "Escritorio";
+        unObjeto2.unEntero = 6;
+        unObjeto3.unaCadena = "Teclado";
+        unObjeto3.unEntero = 7;
+        unObjeto4.unaCadena = "Pantalla";
+        unObjeto4.unEntero = 8; 
+        unObjeto5.unaCadena = "Mouse";
+        unObjeto5.unEntero = 9;
+        
+        ArrayList<PrimeraClase> lista = new ArrayList<>();
+        lista.add(unObjeto1);
+        lista.add(unObjeto2);
+        lista.add(unObjeto3);
+        lista.add(unObjeto4);
+        lista.add(unObjeto5);
+        
+        for(PrimeraClase f : lista){
+        }
+    }
+    
+    public void mostrar(){  
+        System.out.println("Atributos: " + unaCadena + " y " + unEntero);
+    }
+}
